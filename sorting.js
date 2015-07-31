40d51aed6632fa1eaef8774c366ee5653a76e0b8
Daniel Perrelly <danperrelly@gmail.com>
2015-07-31 12:15:42 -0400

    finished mergeSort, merge, and split

--- sorting.js
+++ sorting.js
@@ -19,3 +19,37 @@ function bubbleSort(array){
 	}while(numOfSwaps);
 	return [arr, totalSwaps, totalComparisons];
 }
+
+function mergeSort(array){
+	debugger;
+	if(array.length <= 1) return array;
+	console.log(array.length);
+	var halves = split(array);
+	var left = mergeSort(halves[0]);
+	var right = mergeSort(halves[1]);
+
+	// actual merging occurs here
+	return merge(left, right);
+}
+
+function merge(left, right){
+	var merged = [];
+	while(left.length && right.length){
+		if(left[0] < right[0]){
+			merged.push(left.shift());
+		}
+		else{
+			merged.push(right.shift());
+		}
+	}
+	if(left.length) merged = merged.concat(left);
+	if(right.length) merged = merged.concat(right);
+	return merged;
+}
+
+function split(array){
+	var middle = Math.floor(array.length / 2);
+	var left = array.slice(0, middle);
+	var right = array.slice(middle);
+	return [left, right];
+}
--- sortingSpec.js
+++ sortingSpec.js
@@ -12,3 +12,26 @@ describe('Bubble Sort', function(){
         expect( bubbleSort([0, -2, 5, 72, 37, 923, 7392])[1] ).toEqual( 2 );
     });
 });
+
+describe('Merge Sort', function(){
+    it('handles an empty array', function(){
+        expect( mergeSort([]) ).toEqual( [] );
+    });
+    it('sorts an array', function(){
+        expect( mergeSort([5, 923, 72, 7392, 37, 0, -2] )).toEqual( [-2, 0, 5, 37, 72, 923, 7392] );
+    });
+    it('sorts an array of length 1', function(){
+        expect( mergeSort([5]) ).toEqual( [5] );
+    });
+});
+
+describe('Merge', function(){
+    it('is able to merge two sorted arrays', function(){
+        expect( merge([1, 2, 6], [0, 324, 79834, 9083809]) ).toEqual( [0, 1, 2, 6, 324, 79834, 9083809] );
+    });
+});
+describe('Split Array function', function() {
+  it('is able to split an array into two halves', function() {
+    expect( split([222, 6, 9, 26])).toEqual([[222, 6], [9,26]]);
+  });
+});
